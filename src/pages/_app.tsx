bad2b3f1ebe334122776e7edef120e5a6347e24d
LuckShiba <LuckShiba@protonmail.com>
2023-03-13 01:52:48 -0300

    add: favicon and title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <div className={roboto.className}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Takasaki Studio</title>
       </Head>
       <Component {...pageProps} />
     </div>
