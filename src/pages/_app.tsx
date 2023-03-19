2b48af051ab2d60d43c7a4d9fbd1c623617a1903
LuckShiba <LuckShiba@protonmail.com>
2023-03-19 16:20:25 -0300

    add: meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,26 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Takasaki Studio</title>
+        <meta name="title" content="Takasaki Studio" />
+        <meta
+          name="description"
+          content="Construímos soluções para tornar possível o seu sonho, garantindo sempre a qualidade e um desenvolvimento personalizado para as suas necessidades"
+        />
+        <meta
+          name="keywords"
+          content="desenvolvimento, takasaki, studio, web, programação"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta name="language" content="Portuguese" />
+        <meta property="og:title" content="Takasaki Studio" />
+        <meta property="og:url" content="https://takasaki.studio" />
+        <meta
+          property="og:description"
+          content="Construímos soluções para tornar possível o seu sonho, garantindo sempre a qualidade e um desenvolvimento personalizado para as suas necessidades"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/logo.webp" />
+        <meta property="theme-color" content="#745CFF" />
       </Head>
       <Component {...pageProps} />
     </div>
